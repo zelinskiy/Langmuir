b21ba45e2155b935d1f5814526b94bedaf5133be
Nikita Yurchenko <zelinskiy1917@gmail.com>
2016-10-16 13:45:44 +0300

    upd script + otchet

--- UtilScripts/Program.fs
+++ UtilScripts/Program.fs
@@ -1,5 +1,8 @@
 ﻿open System
 open System.Linq
+open System.Collections.Generic
+
+
 
 let GOMSdict = dict[
                 "K",0.2;
@@ -8,37 +11,51 @@ let GOMSdict = dict[
                 "M",1.35;
                 ]
 
+let dropdownSequence = "EIBEBEEBI"
+
 let DROPDOWNdict = dict[
                     "E","PKPK";
                     "I","PKKK"; 
                     "B","PK";
                     ]
-let stringToList (s:string) = s.ToList() |> Seq.map Char.ToString |> Seq.toList
+
+let tableSequence = "BIBBBBBBIB"
+
+let TABLEdict = dict[
+                    "I","PKKK"; 
+                    "B","PK";
+                    ]
+
+let autocompleteSequence = "KKPKPKKKPKPKKKKPKPKPKKKKKPKPKKKKKKPKPKPKKKPK"
+
+
+let stringToList (s:string) = s.ToList() |> Seq.map Char.ToString |> Seq.toList 
 
 let ruleZero s = (String.concat "" s).Replace("PK", "MPK") |> stringToList
 let ruleOne s = (String.concat "" s).Replace("PMK", "PK") |> stringToList
 
-let rec _dropdownToGOMS acc x =  match x with
+let rec customToGOMS (dict:IDictionary<string, string>) acc x =  match x with
         | [] -> acc
-        | h::t -> _dropdownToGOMS ((DROPDOWNdict.Item h) :: acc) t
-
-let dropdownToGOMS = stringToList >> _dropdownToGOMS []
+        | h::t -> customToGOMS dict ((dict.Item h) :: acc) t
 
 let calculateGOMS = List.map (function(c) -> (c, GOMSdict.Item c))
 
-let showGOMS = List.map (function(s, (v:float)) -> String.concat ":" [s; v.ToString()]) >> String.concat "\n"
+let showGOMS = 
+    List.map (function(s, (v:float)) -> String.concat ":" [s; v.ToString()]) 
+    >> String.concat "\n"
 
-let sumGOMS = List.map (function((s:string), (v:float)) -> v) >> List.reduce (+)
+let sumGOMS = 
+    List.map (function((s:string), (v:float)) -> v) 
+    >> List.reduce (+)
 
 [<EntryPoint>]
 let main argv = 
-    "EIBEBEEBI"
-    |> dropdownToGOMS
+    autocompleteSequence
+    |> stringToList 
+    //|> customToGOMS DROPDOWNdict []
     |> ruleZero
     |> ruleOne    
     |> calculateGOMS
-    |> function(g) -> (showGOMS g , sumGOMS g)
-    |> function(a, b) -> printf "%A\n\n" b; (a,b)
-    |> function(a, b) -> printf "%A" a; (a,b)
+    |> function(g) -> printf "%A\n\n" (sumGOMS g); printf "%A\n\n" (showGOMS g)
     Console.ReadLine() |> ignore
     0
